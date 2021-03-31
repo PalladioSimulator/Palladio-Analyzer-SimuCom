f84d5a5ad339ecedc87d09d61269ff1eccb01e68
Jonas Lehmann <77493291+jolehmann@users.noreply.github.com>
2021-04-01 00:32:27 +0200

    Fixed the calculation of the modified demand of AbsSchedRes

---
+++ bundles/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/DemandModificationDTO.java
@@ -0,0 +1,28 @@
+package de.uka.ipd.sdq.simucomframework.resources;
+
+public class DemandModificationDTO {
+
+	private final double scaledDemand;
+
+	private final double additiveDemandValue;
+
+	public DemandModificationDTO(double scaledDemand, double additiveDemandValue) {
+		this.scaledDemand = scaledDemand;
+		this.additiveDemandValue = additiveDemandValue;
+	}
+	
+	/**
+	 * @return the scaled demand
+	 */
+	public double getScaledDemand() {
+		return scaledDemand;
+	}
+
+	/**
+	 * @return an additive demand of time units (delay, latency)
+	 */
+	public double getAdditiveDemandValue() {
+		return additiveDemandValue;
+	}
+	
+}
--- bundles/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/DemandModifyingBehavior.java
+++ bundles/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/DemandModifyingBehavior.java
@@ -4,42 +4,53 @@ import de.uka.ipd.sdq.simucomframework.variables.StackContext;
 import de.uka.ipd.sdq.simucomframework.variables.converter.NumberConverter;
 
 /**
- * A DemandModifyingBehavior has a scalingFactor for the throughput or processingrate of the resource and a value for the delay or latency.
- * AbstractScheduledResources use this behavior to modify the demand of time units for a computation.
+ * A DemandModifyingBehavior has a scalingFactor for the throughput or
+ * processingrate of the resource and a value for the delay or latency.
+ * AbstractScheduledResources use this behavior to modify the demand of time
+ * units for a computation.
  * 
  * 
  * @author Jonas Lehmann
  *
  */
 public class DemandModifyingBehavior {
-	
-	private final String scalingFactor; 	//of throughput or processingrate
-    private final String delay; 			//or latency of resource
-    
-    /**
-     * Creates a new {@link DemandModifyingBehavior}.
-     * @param scalingFactor 	value for scaling throughput or processingrate
-     * @param delay 			value for delay or latency of the resource
-     */
-    public DemandModifyingBehavior(final String scalingFactor, final String delay) {
-    	this.scalingFactor = scalingFactor;
-    	this.delay = delay;
-    }
-	
+
+	private final String scalingFactor; // of throughput or processingrate
+	private final String delay; // or latency of resource
+
 	/**
-	 * Calculates an additive demand of time units:
-	 * additiveValue = prevDemand / scaleFactor + delay - prevDemand.
+	 * Creates a new {@link DemandModifyingBehavior}.
 	 * 
-	 * @param previousDemand demand value before modification
-	 * @return an additive value to get the newDemand by adding it to the prevDemand
+	 * @param scalingFactor value for scaling throughput or processingrate
+	 * @param delay         value for delay or latency of the resource
 	 */
-	public double getAdditiveDemandValue(double previousDemand) {
+	public DemandModifyingBehavior(final String scalingFactor, final String delay) {
+		this.scalingFactor = scalingFactor;
+		this.delay = delay;
+	}
+
+	/**
+	 * Scales a demand with the scalingFactor (newDemand = prevDemand /
+	 * scalingFactor). Returns the scaled demand and an additive demand of time
+	 * units (delay, latency).
+	 * 
+	 * @param previousDemand
+	 * @return A Demand Modification Data Transfer Object which store the two
+	 *         values.
+	 */
+	public DemandModificationDTO modifyDemand(double previousDemand) {
+		return new DemandModificationDTO(this.scaleDemand(previousDemand), this.getAdditiveDemandValue());
+	}
+
+	private double scaleDemand(double previousDemand) {
 		final double scaling = NumberConverter.toDouble(StackContext.evaluateStatic(scalingFactor));
-		if(scaling <= 0) {
+		if (scaling <= 0) {
 			throw new RuntimeException("ScalingFactor of ressources processingrate/throughput was less or equal zero");
 		}
-		final double scaledDemand = previousDemand / scaling;
-        final double additiveDemandValue = NumberConverter.toDouble(StackContext.evaluateStatic(delay)) + (scaledDemand - previousDemand);
-		return additiveDemandValue;
+		return previousDemand / scaling;
+	}
+
+	private double getAdditiveDemandValue() {
+		return NumberConverter.toDouble(StackContext.evaluateStatic(delay));
 	}
 }
